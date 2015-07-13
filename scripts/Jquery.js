83b43a6e5c4569ffe146ce26108c12a4f0b1ca63
TheBigBoss840 <shaunporter96@gmail.com>
2015-07-13 16:10:04 +0100

    First Version of the Porter Designs Website

---
+++ scripts/Jquery.js
@@ -0,0 +1,11 @@
+/** Used for smooth animation between internal links **/
+
+$(document).ready(function() {
+	$('a').click(function(){
+   		$('html, body').animate({
+        	scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+    	}, 500);
+    	return false;
+	});
+});
+
